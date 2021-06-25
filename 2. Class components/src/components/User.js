c254c3bf4b936e568106631250d5b564df16273d
Pranav <pranavmendi@gmail.com>
2021-06-25 18:03:34 +0530

    Add Class Components dir

---
+++ 2. Class components/src/components/User.js
@@ -0,0 +1,14 @@
+import { component } from 'react';
+import classes from './User.module.css';
+
+class User extends component {
+	render() {
+		return <li className={classes.user}>{this.props.name}</li>;
+	}
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
