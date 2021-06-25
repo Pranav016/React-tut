6af306bc58228fce30cb2cbf2fece3892b0d7fd7
Pranav <pranavmendi@gmail.com>
2021-06-25 20:33:19 +0530

    Add AddMovie component and create Firebase DB to store movies

--- 1. HTTP req and APIs/src/App.js
+++ 1. HTTP req and APIs/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useCallback } from 'react';
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -8,13 +8,13 @@ function App() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	// useEffect
-
-	const fetchMovieHandler = async () => {
+	const fetchMovieHandler = useCallback(async () => {
 		try {
 			setError(null);
 			setIsLoading(true);
-			const response = await fetch('https://swapi.dev/api/films/');
+			const response = await fetch(
+				'https://react-tut-a0f33-default-rtdb.firebaseio.com/moves.json'
+			);
 			if (!response.ok) {
 				throw new Error('Something went wrong!');
 			}
@@ -32,7 +32,16 @@ function App() {
 			setError(err.message);
 		}
 		setIsLoading(false);
-	};
+	}, []);
+
+	// hoisting doesn't work when function stored in a variable
+	useEffect(() => {
+		fetchMovieHandler();
+	}, [fetchMovieHandler]);
+
+	function addMovieHandler(movie) {
+		console.log(movie);
+	}
 
 	let content = <p>No movies found</p>;
 
@@ -41,7 +50,7 @@ function App() {
 	}
 
 	if (error) {
-		content = error;
+		content = <p>{error}</p>;
 	}
 
 	if (isLoading) {
@@ -50,6 +59,9 @@ function App() {
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
 			<section>
 				<button onClick={fetchMovieHandler}>Fetch Movies</button>
 			</section>
