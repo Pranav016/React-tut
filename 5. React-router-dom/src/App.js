9200bdbc66abbf95102ffadbbd3fb3388cb2aff9
Pranav <pranavmendi@gmail.com>
2021-06-30 11:07:52 +0530

    Add Welcome and Products pages, MainHeader component and Link using router-dom

--- 5. React-router-dom/src/App.js
+++ 5. React-router-dom/src/App.js
@@ -1,8 +1,17 @@
+import { Route } from 'react-router-dom';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
+import MainHeader from './components/MainHeader';
+
 function App() {
 	return (
-		<div>
-			<h2>Let's get started!</h2>
-		</div>
+		<>
+			<MainHeader />
+			<main>
+				<Route path='/welcome' component={Welcome} />
+				<Route path='/products' component={Products} />
+			</main>
+		</>
 	);
 }
 
